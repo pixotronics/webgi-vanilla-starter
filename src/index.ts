bf7fc21bdf46b26d37f408121b224b1daa3db0ea
Palash Bansal <palashbansal96@gmail.com>
2022-04-13 01:22:28 +0530

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import {
+    ViewerApp,
+    AssetManagerPlugin,
+    GBufferPlugin,
+    ProgressivePlugin,
+    TonemapPlugin,
+    SSRPlugin,
+    SSAOPlugin,
+    DiamondPlugin,
+    FrameFadePlugin,
+    GLTFAnimationPlugin,
+    GroundPlugin,
+    BloomPlugin,
+    TemporalAAPlugin,
+    AnisotropyPlugin,
+
+    addBasePlugins, ITexture,
+} from "webgi";
+import "./styles.css";
+
+async function setupViewer(){
+
+    // Initialize the viewer
+    const viewer = new ViewerApp({
+        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+        useRgbm: true,
+    })
+
+    // Add some plugins
+    const manager = await viewer.addPlugin(AssetManagerPlugin)
+    await viewer.addPlugin(GBufferPlugin)
+    await viewer.addPlugin(new ProgressivePlugin(32))
+    await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+    await viewer.addPlugin(SSRPlugin)
+    await viewer.addPlugin(SSAOPlugin)
+    await viewer.addPlugin(DiamondPlugin)
+    await viewer.addPlugin(FrameFadePlugin)
+    await viewer.addPlugin(GLTFAnimationPlugin)
+    await viewer.addPlugin(GroundPlugin)
+    await viewer.addPlugin(BloomPlugin)
+    await viewer.addPlugin(TemporalAAPlugin)
+    await viewer.addPlugin(AnisotropyPlugin)
+
+    // or use this to add all main ones at once.
+    // addBasePlugins(viewer)
+
+    viewer.renderer.refreshPipeline()
+
+    await manager.addFromPath("./assets/scene.glb")
+
+    await viewer.scene.setEnvironment(
+        await manager.importer!.importSinglePath<ITexture>(
+            "./assets/environment.hdr"
+        )
+    );
+
+
+}
+
+setupViewer()
