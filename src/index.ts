7434fdd4dfa100bf7ebafe86b77cf32f5c4b4872
Palash Bansal <palashbansal96@gmail.com>
2022-07-25 13:55:18 +0530

    WebGi package update and disabled orbit controls.

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,14 @@ async function setupViewer(){
 
     await manager.addFromPath("./assets/camera.glb")
 
-    const camera = viewer.scene.activeCamera.cameraObject
-    const controls = viewer.scene.activeCamera.controls!
+    const camera = viewer.scene.activeCamera
+    if(camera.controls) camera.controls.enabled = false
 
     await timeout(50) // Wait 50ms
 
     gsap.timeline()
     .fromTo(camera.position,{x: 3.6, y: -0.04, z: -3.93}, {x: -3.6, y: -0.04, z: -3.93, duration: 4, delay: 1, onUpdate: updatePositions})
-    .fromTo(controls.target, {x: 8.16, y: -0.13, z: 0.51}, {x: 0.16, y: -0.13, z: 0.51, duration: 4, onUpdate: updatePositions }, '-=4')
+    .fromTo(camera.target, {x: 8.16, y: -0.13, z: 0.51}, {x: 0.16, y: -0.13, z: 0.51, duration: 4, onUpdate: updatePositions }, '-=4')
     .fromTo('.header--container', {opacity: 0, y: '-100%'}, {opacity: 1, y: '0%', ease: "power1.inOut", duration: 0.8}, '-=1')
     .fromTo('.hero--content', {opacity: 0, x: '-50%'}, {opacity: 1, x: '0%', ease: "power4.inOut", duration: 1.8, onComplete: setupScrollAnimation}, '-=1')
 
@@ -43,8 +43,9 @@ async function setupViewer(){
             scrollTrigger: { trigger: ".cam-view-2",  start: "top bottom", end: "top top", scrub: true, immediateRender: false},
             onUpdate: updatePositions
         })
-        .to(controls.target,{x: -0.6, y: -0.1, z: 0.9,
-            scrollTrigger: { trigger: ".cam-view-2",  start: "top bottom", end: "top top", scrub: true, immediateRender: false }
+        .to(camera.target,{x: -0.6, y: -0.1, z: 0.9,
+            scrollTrigger: { trigger: ".cam-view-2",  start: "top bottom", end: "top top", scrub: true, immediateRender: false },
+            onUpdate: updatePositions
         })
         .to('.hero--content', {opacity: 0, xPercent: '-100', ease: "power4.out",
             scrollTrigger: { trigger: ".cam-view-2", start: "top bottom", end: "top top", scrub: 1, immediateRender: false, pin: '.hero--content'
@@ -55,11 +56,12 @@ async function setupViewer(){
 
         // // POWER SECTION
         .to(camera.position,  {x: -0.07, y: 5.45, z: -3.7,
-            scrollTrigger: { trigger: ".cam-view-3",  start: "top bottom", end: "top top", scrub: true, immediateRender: false }
-            , onUpdate: updatePositions
+            scrollTrigger: { trigger: ".cam-view-3",  start: "top bottom", end: "top top", scrub: true, immediateRender: false },
+            onUpdate: updatePositions
         })
-        .to(controls?.target, {x: -0.04, y: -0.52, z: 0.61,
-            scrollTrigger: { trigger: ".cam-view-3",  start: "top bottom", end: "top top", scrub: true, immediateRender: false }
+        .to(camera.target, {x: -0.04, y: -0.52, z: 0.61,
+            scrollTrigger: { trigger: ".cam-view-3",  start: "top bottom", end: "top top", scrub: true, immediateRender: false },
+            onUpdate: updatePositions
         })
         .to('.performance--content', {autoAlpha: 0, ease: "power4.out",
             scrollTrigger: { trigger: ".cam-view-3", start: "top bottom", end: 'top center', scrub: 1, immediateRender: false,
@@ -76,8 +78,9 @@ async function setupViewer(){
             scrollTrigger: { trigger: ".cam-view-4",  start: "top bottom", end: "top top", scrub: true, immediateRender: false },
             onUpdate: updatePositions
         })
-        .to(controls?.target, {x: 0.04, y: 0.2, z: 0.6,
-            scrollTrigger: { trigger: ".cam-view-4",  start: "top bottom", end: "top top", scrub: true, immediateRender: false }
+        .to(camera.target, {x: 0.04, y: 0.2, z: 0.6,
+            scrollTrigger: { trigger: ".cam-view-4",  start: "top bottom", end: "top top", scrub: true, immediateRender: false },
+            onUpdate: updatePositions
         })
         .fromTo('.autofocus--content', {opacity: 0, y: '130%'}, {opacity: 1, y: '0%', duration: 0.5, ease: "power4.out",
             scrollTrigger: { trigger: ".cam-view-4", start: "top 20%", end: "top top", scrub: 1, immediateRender: false
@@ -86,7 +89,8 @@ async function setupViewer(){
     }
 
     function updatePositions(){
-        viewer.scene.activeCamera.setDirty()
+        camera.positionUpdated(false)
+        camera.targetUpdated(true)
     }
 
 
