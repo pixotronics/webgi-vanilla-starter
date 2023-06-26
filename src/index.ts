1146589c35d1b0016dc91245d0f21b41f3c57274
Palash Bansal <palashbansal96@gmail.com>
2023-06-26 20:32:48 +0530

    Version update.

--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,7 @@ async function setupViewer(){
     // await viewer.addPlugin(BloomPlugin)
     // await viewer.addPlugin(TemporalAAPlugin)
     // await viewer.addPlugin(AnisotropyPlugin)
+    // and many more...
 
     // or use this to add all main ones at once.
     await addBasePlugins(viewer)
@@ -63,14 +64,11 @@ async function setupViewer(){
     // This must be called once after all plugins are added.
     viewer.renderer.refreshPipeline()
 
-    await manager.addFromPath("./assets/classic-watch.glb")
+    // Import and add a GLB file.
+    await viewer.load("./assets/classic-watch.glb")
 
     // Load an environment map if not set in the glb file
-    // await viewer.scene.setEnvironment(
-    //     await manager.importer!.importSinglePath<ITexture>(
-    //         "./assets/environment.hdr"
-    //     )
-    // );
+    await viewer.setEnvironmentMap("./assets/environment.hdr");
 
     // Add some UI for tweak and testing.
     const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
