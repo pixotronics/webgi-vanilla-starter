5f1de0a63a8d3aea37c19e45ec5fc809389d0d1e
Palash Bansal <palashbansal96@gmail.com>
2023-06-27 15:19:06 +0530

    Upgrade version

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ async function setupViewer(){
     viewer.renderer.refreshPipeline()
 
     // Import and add a GLB file.
-    await manager.addFromPath("./assets/classic-watch.glb")
+    await viewer.load("./assets/classic-watch.glb")
 
     // Load an environment map if not set in the glb file
     // await viewer.setEnvironmentMap((await manager.importer!.importSinglePath<ITexture>("./assets/environment.hdr"))!);
