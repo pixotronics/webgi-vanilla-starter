461895de5f26e00644e15e6e03dd70034d9095c0
Sushil Kumar Shah <sushil.shah@broadcom.com>
2023-08-05 09:26:17 +0530

    ॐ ।। श्रीरामः ।।

--- src/index.js
+++ src/index.js
@@ -17,14 +17,15 @@ async function setupViewer(){
     // Initialize the viewer
     const viewer = new ViewerApp({
         canvas: document.getElementById('webgi-canvas'),
+        useRgbm: false,
     });
 
-    // const data={
-    //     position: {x: 0, y: 0, z: 0},
-    //     rotation: {x: 0, y: 0, z: 0},
-    // }
+    const data={
+        position: {x: 0, y: 0, z: 0},
+        rotation: {x: 0, y: 0, z: 0},
+    }
 
-    // const pane = new Pane();
+    const pane = new Pane();
     viewer.renderer.displayCanvasScaling = Math.min(window.devicePixelRatio, 1)
 
     const manager = await viewer.addPlugin(AssetManagerPlugin)
@@ -34,14 +35,14 @@ async function setupViewer(){
 
     const importer = manager.importer
 
-    // importer.addEventListener("onProgress", (ev)=> {
-    //     const progress=ev.loaded/ev.total
-    //     document.querySelector(".progress").setAttribute("style", `transform: scaleX(${progress})`)
-    // });
+    importer.addEventListener("onProgress", (ev)=> {
+        const progress=ev.loaded/ev.total
+        document.querySelector(".progress").setAttribute("style", `transform: scaleX(${progress})`)
+    });
 
-    // importer.addEventListener("onLoad", () => {
-    //     introAnimation();
-    // });
+    importer.addEventListener("onLoad", () => {
+        introAnimation();
+    });
 
     // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
     await viewer.addPlugin(CanvasSnipperPlugin)
@@ -52,19 +53,12 @@ async function setupViewer(){
     // Import and add a GLB file.
     // await viewer.load("./assets/scene.glb")
     const model = await manager.addFromPath("./assets/ring-scene1.glb");
-    // const object3d = model[0].modelObject;
-    // const modelPosition = object3d.position;
-    // const modelRotation = object3d.rotation;
+    const object3d = model[0].modelObject;
+    const modelPosition = object3d.position;
+    const modelRotation = object3d.rotation;
 
-    // const loaderElement = document.querySelector(".loader");
+    const loaderElement = document.querySelector(".loader");
 
-    // Load an environment map if not set in the glb file
-    // await viewer.setEnvironmentMap((await manager.importer!.importSinglePath<ITexture>("./assets/environment.hdr"))!);
-
-    // Add some UI for tweak and testing.
-    // const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    // uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
     function introAnimation() {
         const t1=gsap.timeline()
 
@@ -77,151 +71,175 @@ async function setupViewer(){
         })
     }
 
-    // pane.addInput(data, "position", {
-    //     x: { step: 0.01 },
-    //     y: { step: 0.01 },
-    //     z: { step: 0.01 },
-    // });
-    // pane.addInput(data, "rotation", {
-    //     x: { min: -6.28, max: 6.28, step: 0.001 },
-    //     y: { min: -6.28, max: 6.28, step: 0.001 },
-    //     z: { min: -6.28, max: 6.28, step: 0.001 },
-    // });
+    pane.addInput(data, "position", {
+        x: { step: 0.01 },
+        y: { step: 0.01 },
+        z: { step: 0.01 },
+    });
+    pane.addInput(data, "rotation", {
+        x: { min: -6.28, max: 6.28, step: 0.001 },
+        y: { min: -6.28, max: 6.28, step: 0.001 },
+        z: { min: -6.28, max: 6.28, step: 0.001 },
+    });
 
-    // pane.on("change", (e) => {
-    //     if (e.presetKey === "rotation") {
-    //         const {x, y, z } = e.value;
-    //         modelRotation.set(x, y, z);
-    //     } else {
-    //         const {x, y, z } = e.value;
-    //         modelPosition.set(x, y, z);
-    //     }
-    //     onUpdate();
-    // })
+    pane.on("change", (e) => {
+        if (e.presetKey === "rotation") {
+            const {x, y, z } = e.value;
+            modelRotation.set(x, y, z);
+        } else {
+            const {x, y, z } = e.value;
+            modelPosition.set(x, y, z);
+        }
+        onUpdate();
+    })
 
-    // function setupScrollAnimation() {
-    //     // document.body.removeChild(loaderElement);
+    function setupScrollAnimation() {
+        // document.body.removeChild(loaderElement);
 
-    //     const tl = gsap.timeline();
+        const tl = gsap.timeline();
 
-    //     tl.to(modelPosition, {
-    //     x: -0.9,
-    //     y: -0.43,
-    //     z: 0,
-    //     scrollTrigger: {
-    //         trigger: ".first",
-    //         start: "top top",
-    //         end: "top top",
-    //         scrub: 0.2,
-    //         immediateRender: false,
-    //     },
-    //     onUpdate,
-    //     })
-    //     .to(modelPosition, {
-    //         x: -1.36,
-    //         y: -0.02,
-    //         z: -0.22,
-    //         scrollTrigger: {
-    //         trigger: ".second",
-    //         start: "top bottom",
-    //         end: "top top",
-    //         scrub: 0.2,
-    //         immediateRender: false,
-    //         },
-    //         onUpdate,
-    //     })
-    //     .to(modelRotation, {
-    //         x: 0.0,
-    //         y: 0,
-    //         z: -1.57,
-    //         scrollTrigger: {
-    //         trigger: ".second",
-    //         start: "top bottom",
-    //         end: "top top",
-    //         scrub: 0.2,
-    //         immediateRender: false,
-    //         },
-    //     })
-    //     .to(modelPosition, {
-    //         x: 0.38,
-    //         y: -0.11,
-    //         z: -1.06,
-    //         scrollTrigger: {
-    //           trigger: ".third",
-    //           start: "top bottom",
-    //           end: "top top",
-    //           scrub: 0.2,
-    //           immediateRender: false,
-    //         },
-    //         onUpdate,
-    //       })
+        tl.to(modelPosition, {
+        x: -0.9,
+        y: -0.43,
+        z: 0,
+        scrollTrigger: {
+            trigger: ".first",
+            start: "top top",
+            end: "top top",
+            scrub: 0.2,
+            immediateRender: false,
+        },
+        onUpdate,
+        })
+        .to(modelPosition, {
+            x: 0.54,
+            y: -0.27,
+            z: 2.87,
+            scrollTrigger: {
+            trigger: ".second",
+            start: "top bottom",
+            end: "top top",
+            scrub: 0.2,
+            immediateRender: false,
+            },
+            onUpdate,
+        })
+        .to(modelRotation, {
+            x: 0.088,
+            y: -0.288,
+            z: -0.097,
+            scrollTrigger: {
+            trigger: ".second",
+            start: "top bottom",
+            end: "top top",
+            scrub: 0.2,
+            immediateRender: false,
+            },
+        })
+        .to(modelPosition, {
+            x: -1.21,
+            y: -0.40,
+            z: 1.00,
+            scrollTrigger: {
+              trigger: ".third",
+              start: "top bottom",
+              end: "top top",
+              scrub: 0.2,
+              immediateRender: false,
+            },
+            onUpdate,
+          })
     
-    //       .to(modelRotation, {
-    //         x: 0.403,
-    //         y: 0.957,
-    //         z: -0.421,
-    //         scrollTrigger: {
-    //           trigger: ".third",
-    //           start: "top bottom",
-    //           end: "top top",
-    //           scrub: 0.2,
-    //           immediateRender: false,
-    //         },
-    //       })
-    //       .to(modelPosition, {
-    //         x: 0.00,
-    //         y: -0.72,
-    //         z: 1.31,
-    //         scrollTrigger: {
-    //           trigger: ".fourth",
-    //           start: "top bottom",
-    //           end: "top top",
-    //           scrub: 0.2,
-    //           immediateRender: false,
-    //         },
-    //         onUpdate,
-    //       })
-    
-    //       .to(modelRotation, {
-    //         x: -0.120,
-    //         y: 1.598,
-    //         z: 0.132,
-    //         scrollTrigger: {
-    //           trigger: ".fourth",
-    //           start: "top bottom",
-    //           end: "top top",
-    //           scrub: 0.2,
-    //           immediateRender: false,
-    //         },
-    //       })
-    //       .to(".section--one--container1", {
-    //         opacity: 0,
-    //         scrollTrigger: {
-    //           trigger: ".section--one--container1",
-    //           start: "top top",
-    //           end: "bottom top",
-    //           scrub: true,
-    //           immediateRender: false,
-    //         },
-    //       })
-    //       .to(".section--one--container2", {
-    //         opacity: 0,
-    //         scrollTrigger: {
-    //           trigger: ".second",
-    //           start: "top bottom",
-    //           end: "top center",
-    //           scrub: true,
-    //           immediateRender: false,
-    //         },
-    //       })
+          .to(modelRotation, {
+            x: 0.300,
+            y: 0.650,
+            z: -0.131,
+            scrollTrigger: {
+              trigger: ".third",
+              start: "top bottom",
+              end: "top top",
+              scrub: 0.2,
+              immediateRender: false,
+            },
+          })
+          .to(modelPosition, {
+            x: 0.00,
+            y: -0.77,
+            z: 2,
+            scrollTrigger: {
+              trigger: ".fourth",
+              start: "top bottom",
+              end: "top top",
+              scrub: 0.2,
+              immediateRender: false,
+            },
+            onUpdate,
+          })
+          .to(modelRotation, {
+            x: -0.4,
+            y: 0,
+            z: 0,
+            scrollTrigger: {
+              trigger: ".fourth",
+              start: "top bottom",
+              end: "top top",
+              scrub: 0.2,
+              immediateRender: false,
+            },
+          })
+          .to(modelPosition, {
+            x: 0.00,
+            y: -0.35,
+            z: 0.00,
+            scrollTrigger: {
+              trigger: ".fifth",
+              start: "top bottom",
+              end: "top top",
+              scrub: 0.2,
+              immediateRender: false,
+            },
+            onUpdate,
+          })
+          .to(modelRotation, {
+            x: -0.768,
+            y: 0,
+            z: 0,
+            scrollTrigger: {
+              trigger: ".fifth",
+              start: "top bottom",
+              end: "top top",
+              scrub: 0.2,
+              immediateRender: false,
+            },
+          })
+          .to(".section--one--container1", {
+            opacity: 0,
+            scrollTrigger: {
+              trigger: ".section--one--container1",
+              start: "top top",
+              end: "bottom top",
+              scrub: true,
+              immediateRender: false,
+            },
+          })
+          .to(".section--one--container2", {
+            opacity: 0,
+            scrollTrigger: {
+              trigger: ".second",
+              start: "top bottom",
+              end: "top center",
+              scrub: true,
+              immediateRender: false,
+            },
+          })
           
-    //     console.log("setupScrollAnimation");
+        console.log("setupScrollAnimation");
 
-    // }
+    }
 
-    // function onUpdate(){
-    //     viewer.setDirty();
-    // }
+    function onUpdate(){
+        viewer.setDirty();
+    }
 
 }
 
